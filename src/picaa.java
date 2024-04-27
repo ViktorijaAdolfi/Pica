7198c8a9b092e6b166a7b6a50e3ebc5b621c23a2
Viktorija <viktorijadolfi@gmail.com>
2024-04-27 10:28:54 +0300

    Pamainīju sākumu un ieviesu case

--- src/picaa.java
+++ src/picaa.java
@@ -1,8 +1,28 @@
+import java.util.Arrays;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 public class picaa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ImageIcon vikasPizzeria = new ImageIcon(picaa.class.getResource("/img/vikasPizzeria.png"));
+		String izvele;
+		int caseS;
+		String[] saakums = {"Veikt pasūtījumu","Apturēt programmu"};
+		
+		JOptionPane.showMessageDialog(null, "", "Laipni lūgti.. ", 
+                JOptionPane.QUESTION_MESSAGE, vikasPizzeria);
+		
+		do {
+    		izvele = (String)JOptionPane.showInputDialog(null, "Izvēlies darbību!", "Start!",
+					JOptionPane.QUESTION_MESSAGE, null, saakums, saakums[0]);
+    		
+    		if(izvele == null)
+    			izvele = "Apturēt programmu";
+    		
+		}while(!izvele.equals("Apturēt programmu"));
+		//beidz programmas darbību!!	
 
 	}
 
