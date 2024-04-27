64cc7d21745f2f32c7a36e724a9d6628909910b6
Viktorija <viktorijadolfi@gmail.com>
2024-04-27 11:13:39 +0300

    izveidoju metodi aprekinat cenu

--- src/picaa.java
+++ src/picaa.java
@@ -1,15 +1,60 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class picaa {
+	
+	class Pizza {
+	    private String izmers;
+	    private ArrayList<String> tops;
+	    private ArrayList<String> sauce;
+	    private double cena;
+
+	    public Pizza(String izmers, ArrayList<String> tops, ArrayList<String> sauce) {
+	        this.izmers = izmers;
+	        this.tops = tops;
+	        this.sauce = sauce;
+	        this.cena = aprekinatCenu();
+	    }
+	    
+	class Pasutijums{
+		private String vards;
+		private String adrese;
+	    private String tel;
+	    private Pizza pica;
+	    
+	    public Pasutijums (String vards, String adrese, String tel, Pizza pica) {
+	        this.vards = vards;
+	        this.adrese = adrese;
+	        this.tel = tel;
+	        this.pica = pica;
+	    }
+	    
+	    public String getVards() {
+	    	return vards;
+	    }
+	    public String getAdrese() {
+	    	return adrese;
+	    }
+	    public String getTel() {
+	    	return tel;
+	    }
+	    public Pizza getPica() {
+	    	return pica;
+	    }
+	    
+	    @Override
+	    public String toString() {
+	        return "Vārds: "+vards+"\nAdrese: "+adrese+"\nTel. num: "+tel+"\nPica: "+pica;
+	    }
+	}
 
 	public static void main(String[] args) {
 		ImageIcon vikasPizzeria = new ImageIcon(picaa.class.getResource("/img/vikasPizzeria.png"));
 		String izvele;
 		int caseS;
-		String[] saakums = {"Veikt pasūtījumu","Apturēt programmu"};
+		String[] saakums = {"Veikt pasūtījumu","Apskatīt pasūtījumus","Apturēt programmu"};
 		
 		JOptionPane.showMessageDialog(null, "", "Laipni lūgti.. ", 
                 JOptionPane.QUESTION_MESSAGE, vikasPizzeria);
@@ -23,7 +68,11 @@ public class picaa {
     		
     		switch(izvele) {
     		case "Veikt pasūtījumu":
+    			//pasutit();
+    			break;
     			
+    		case "Apskatīt pasūtījumus":
+    			//pasūtījumi
     			break;
     			
     		case "Apturēt programmu":
@@ -32,7 +81,6 @@ public class picaa {
     		}
     		
 		}while(!izvele.equals("Apturēt programmu"));
-		//beidz programmas darbību!!	
 
 	}
 
